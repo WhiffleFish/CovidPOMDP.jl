cc5c931abdf59cd774c7b56e3669f709a2824f90
WhiffleFish <tylertronics@gmail.com>
2022-03-10 16:54:24 -0700

    Proportinal policy + POMDP testing

--- src/proportional_control.jl
+++ src/proportional_control.jl
@@ -1,22 +1,37 @@
-struct ProportionalControlSolver
+struct ProportionalControlSolver <: Solver
     k::Float64
 end
 
-ProportionalControlSolver() = ProportionalControlSolver(0.5)
+ProportionalControlSolver() = ProportionalControlSolver(1.0)
 
-struct ProportionalControlPlanner
+struct ProportionalControlPlanner <: Policy
     k::Float64
+end
+
+function POMDPs.solve(pc::ProportionalControlSolver, pomdp::CovidPOMDP)
+    return ProportionalControlPlanner(pc.k)
 end
 
 function BasicPOMCP.convert_estimator(pc::ProportionalControlSolver, ::Any, pomdp::CovidPOMDP)
-    return ProportionalControlPlanner(pc.k)
+    return solve(pc, pomdp)
 end
 
 function POMDPs.action(pc::ProportionalControlPlanner, s::CovidState)
     I = sum(s.I)
     inf_prop = I / (s.S + I + s.R)
-    return CovidAction(pc.k*inf_prop)
+    return CovidAction(min(pc.k*inf_prop, 1.0))
 end
+
+function POMDPs.action(pc::ProportionalControlPlanner, b::ParticleCollection{CovidState})
+    s̄ = Statistics.mean(b.particles)
+    return POMDPs.action(pc, s̄)
+end
+
+function POMDPs.action(pc::ProportionalControlPlanner, b)
+    b = ParticleCollection([rand(b) for _ in 1:100]) # TODO: change default particle size
+    return POMDPs.action(pc, b)
+end
+
 
 function BasicPOMCP.estimate_value(
     estimator::ProportionalControlPlanner,
--- test/simulation.jl
+++ test/simulation.jl
@@ -1,5 +1,5 @@
 
-@testset "Simulation" begin
+@testset "MDP Simulation" begin
     pomdp = CovidPOMDP()
 
     b0 = initialstate(pomdp)
@@ -23,4 +23,31 @@
     @test all(==(N), S[i] + I[i] + R[i] for i in 1:T)
 
     # @testinferred CovidPOMDPs.sim_step(pomdp, s0, a)
+end
+
+@testset "POMDP Simulation" begin
+    pomdp = CovidPOMDP()
+    sol = CovidPOMDPs.ProportionalControlSolver()
+    planner = solve(sol, pomdp)
+
+    b0 = initialstate(pomdp, Distributions.Uniform(1,10_000))
+    s0 = rand(b0)
+    a = rand(actions(pomdp))
+
+    hist = simulate(pomdp, s0)
+    S = hist.sus
+    I = hist.inf
+    R = hist.rec
+    N = hist.N
+    T = hist.T
+
+    # nonnegativity
+    @test all(≥(0), S)
+    @test all(≥(0), I)
+    @test all(≥(0), R)
+    @test all(≥(0), hist.pos_test)
+    @test all( 0.0 .≤ getfield.(hist.actions,:testing_prop) .≤ 1.0)
+
+    # Population size preservation
+    @test all(==(N), S[i] + I[i] + R[i] for i in 1:T)
 end
